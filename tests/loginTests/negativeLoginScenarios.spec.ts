d21dbda9fd1ae322a81f93e56d412f5177799106
walidatorkh <walidatorkh@gmail.com>
2024-08-13 16:45:53 +0300

    Refactoring P/N scenarios

--- tests/loginTests/negativeLoginScenarios.spec.ts
+++ tests/loginTests/negativeLoginScenarios.spec.ts
@@ -12,19 +12,19 @@ test.describe('Negative login scenarios', () => {
         
     });
 
-    test('Login with locked_out_user', async ({ page }) => {
+    test('Login with locked_out_user', async () => {
         await loginPage.loginToApplication(process.env.LOCKED_OUT_USER);
         await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_LOCKED_USER)
         await loginPage.validatePageUrl(ApplicationURL.BASE_URL);
     });
 
-    test('Login with incorrect login username', async ({ page }) => {
+    test('Login with incorrect login username', async () => {
         await loginPage.loginToApplication('wrongUser');
         await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_WRONG_USER)
         await loginPage.validatePageUrl(ApplicationURL.BASE_URL);
     });
 
-    test('Login with incorrect password', async ({ page }) => {
+    test('Login with incorrect password', async () => {
         await loginPage.loginToApplication(process.env.STANDARD_USER, 'jopa');
         await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_WRONG_PASSWORD)
         await loginPage.validatePageUrl(ApplicationURL.BASE_URL);
